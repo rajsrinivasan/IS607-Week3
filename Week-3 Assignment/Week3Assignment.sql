373a3b72981f8b2eabec5f93373188c0a51d3f86
RAJAGOPAL SRINIVASAN <raj.70.srinivasan@gmail.com>
2015-02-15 22:28:13 -0600

    IS607-Week3

---
+++ Week-3 Assignment/Week3Assignment.sql
@@ -0,0 +1,10 @@
+﻿
+--- One to Many relationship Query
+Select t1.*,t2.* from orderheader t1, orderdetail t2
+where t1.orderID = t2.orderID;
+
+
+--- Many to Many Relationship Query
+Select t1.*, t2.*, t3.* from orderheader t1, orderdetail t2, productmaster t3
+where t1.orderID = t2.orderID and
+      t2.productID = t3.productID
